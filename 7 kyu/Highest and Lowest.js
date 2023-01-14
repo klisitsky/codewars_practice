a6e6bfc760480e029562dc4f2bb674d2def244f1
Konstantin Lisitsky <kapurelloruru@gmail.com>
2023-01-14 19:51:30 +0100

    Fixed comment exs

--- 7 kyu/Highest and Lowest.js
+++ 7 kyu/Highest and Lowest.js
@@ -1,4 +1,5 @@
-// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// In this little assignment you are given a string of space separated numbers, 
+// and have to return the highest and lowest number.
 
 // Examples
 //   highAndLow("1 2 3 4 5");  // return "5 1"
